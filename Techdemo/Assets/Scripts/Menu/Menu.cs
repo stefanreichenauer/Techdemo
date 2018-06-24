8099b145f96ed30826dc7f87dc064f447f13ec65
Stefan Reichenauer <stefan-reichenauer@gmx.at>
2018-06-24 16:28:53 +0200

    Deleted sharkes

---
+++ Techdemo/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    bool paused;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void pause()
+    {
+        if (!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+        }
+    }
+}
+
